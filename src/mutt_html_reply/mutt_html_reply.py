14d8268fc0a82cd13ab6da08e323eb4ef5faab1c
Erik Rogers <erik@rogers-family.net>
2024-05-29 21:51:44 -0400

    Use argparse module with stdin/stdout support

--- src/mutt_html_reply/mutt_html_reply.py
+++ src/mutt_html_reply/mutt_html_reply.py
@@ -1,3 +1,5 @@
+import argparse
+import email
 import html
 import sys
 from bs4 import BeautifulSoup, Doctype
@@ -8,26 +10,36 @@ HEADER_FIRST_SORT_LIST_COMPARISON = ['F','D','T','C','S']
 def main():
     """
     Create an Outlook-style HTML reply
-    
-    mutt-html-reply [html reply] [html original message] [headers to include in quote] [output path]
     """
 
-    # Get filepaths
-    filepath_reply = sys.argv[1]
-    filepath_original_msg = sys.argv[2]
-    filepath_original_headers = sys.argv[3]
-    filepath_output = sys.argv[4]
+    # Parse args
+    parser = argparse.ArgumentParser(description="Create an Outlook-style HTML reply")
+    parser.add_argument("-m", "--message",
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        help="Original message file")
+    parser.add_argument("-r", "--reply",
+                        type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        help="HTML reply, file or defaults to stdin")
+    parser.add_argument("-o", "--output",
+                        nargs='?',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help="HTML output, file or defaults to stdout")
 
-    # Open files and get text
-    with open(filepath_reply, 'r') as file:
-        html_reply = file.read()
-    with open(filepath_original_msg, 'r') as file:
-        html_original_msg = file.read()
-    with open(filepath_original_headers, 'r') as file:
-        text_original_headers = file.read().splitlines()
+    args = parser.parse_args()
+
+    # Get text/html
+    ## Get the reply html from file/stdin
+    html_reply = args.reply.read()
+
+    ## Get the original headers and html from the original email (rfc822 format)
+    rfc822_original = email.message_from_file(args.message)
+    html_original_msg = _get_message_html(rfc822_original)
+    html_headers = _get_header_html(rfc822_original)
 
     # Convert HTML text to BeautifulSoup object and inline all CSS
-
     bs4_msg = BeautifulSoup(css_inline.inline(html_reply),'html.parser')
 
     bs4_original_msg = BeautifulSoup(css_inline.inline(html_original_msg), 'html.parser')
@@ -38,7 +50,7 @@ def main():
         if isinstance(element, Doctype):
             element.extract()
 
-    bs4_original_headers = BeautifulSoup(_get_header_html(text_original_headers), 'html.parser')
+    bs4_original_headers = BeautifulSoup(html_headers, 'html.parser')
 
     # Combine HTML together
     bs4_final = bs4_msg
@@ -47,23 +59,24 @@ def main():
     bs4_final.body.append(bs4_original_msg) #type: ignore
 
     # Write output
-    with open(filepath_output, 'w') as file:
-        file.write(str(bs4_final))
+    args.output.write(str(bs4_final))
 
 
-def _get_header_html(header_list):
-    resorted_text = []
-    for first in HEADER_FIRST_SORT_LIST_COMPARISON:
-        for header in header_list:
-            if header[0] == first:
-                resorted_text.append(html.escape(header))
-    html_headers = "<p>"
-    for header in resorted_text:
-        html_headers = html_headers + '<br>' + header
-    html_headers = html_headers + "</p>\n"
-    return html_headers
+def _get_header_html(message):
+    #resorted_text = []
+    #for first in HEADER_FIRST_SORT_LIST_COMPARISON:
+    #    for header in header_list:
+    #        if header[0] == first:
+    #            resorted_text.append(html.escape(header))
+    #html_headers = "<p>"
+    #for header in resorted_text:
+    #    html_headers = html_headers + '<br>' + header
+    #html_headers = html_headers + "</p>\n"
+    return "<hr><p>test headers html</p>"
 
 
+def _get_message_html(message):
+    return "<hr><p>test original message html</p>"
 
 if __name__ == "__main__":
     main()
